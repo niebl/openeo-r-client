aa3935666008ecd123d49413cb00cbf06a21e129
niebl <caro.niebl@uni-muenster.de>
2026-02-05 10:50:05 +0100

    move test-api setup to checks

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -12,7 +12,6 @@ test_that("connect with missing url fails when setting via initialization", {
 test_that("connect with valid url", {
   skip_if_offline()
   x = openeo:::OpenEOClient$new(host = "http://127.0.0.1:8080")
-  #x = openeo:::OpenEOClient$new(host = "https://openeo.cloud")
   
   con = x$connect()
   expect(TRUE,failure_message = "connecting got an error")
